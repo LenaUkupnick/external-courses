4f10650778bd011acbd153d1289ecbbec0862dda
Lena Barinova <ukupnickonelove@gmail.com>
2018-10-05 10:00:51 +0300

    task_01.js is done up to the end

--- src/ex2_js-basics-part2/task_01.js
+++ src/ex2_js-basics-part2/task_01.js
@@ -1,8 +1,10 @@
-var a = 45;
+var a = null;
 function typeofArguments(a) {
     if (typeof a == "number")
         return " This is number";
     else if (typeof a == "string")
         return "This is string";
+    else 
+        return "Underfind";
 }
 console.log (typeofArguments(a));
