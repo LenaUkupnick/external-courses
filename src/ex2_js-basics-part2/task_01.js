ff454f0a0a92e01a1ef2e7395acb826295411e40
Lena Barinova <ukupnickonelove@gmail.com>
2018-10-10 23:01:57 +0300

    task-01 is done

--- src/ex2_js-basics-part2/task_01.js
+++ src/ex2_js-basics-part2/task_01.js
@@ -1,10 +1,9 @@
-var a = null;
 function typeofArguments(a) {
-    if (typeof a == "number")
+    if (typeof a == "number") 
         return " This is number";
     else if (typeof a == "string")
         return "This is string";
     else 
         return "Underfind";
 }
-console.log (typeofArguments(a));
+console.log (typeofArguments(45));
---
+++ src/ex3_js-objects-part1/task-01.js
@@ -0,0 +1,8 @@
+var user = {};
+user.name = "Lena";
+user.surname = "Barinova";
+user.city = "Ryazan";
+user.age = "21";
+
+delete user.city;
+console.log (user);
