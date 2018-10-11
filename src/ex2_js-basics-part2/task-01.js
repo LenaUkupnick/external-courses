4841032e7a760a7254523b34f3dba955921c82be
LenaUkupnick <43740817+LenaUkupnick@users.noreply.github.com>
2018-10-11 09:55:49 +0300

    Update task-01.js

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -1,10 +1,13 @@
 function typeofArguments(a) {
-    if (typeof a === "number") 
+    if (typeof a === "number"){ 
         return "number";
-    else if (typeof a === "string")
+    }
+    else if (typeof a === "string") {
         return "string";
-        else if ( typeof a === "otherwise")
+    }
+        else if ( typeof a === "otherwise"){
         return "underfined";
+        }
 }
 console.log (typeofArguments(35));
 module.exports = typeofArguments;
