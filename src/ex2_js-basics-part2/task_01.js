ee948beb7e22f713b82b2e2078cd21c10118984d
Lena Barinova <ukupnickonelove@gmail.com>
2018-10-11 13:25:07 +0300

    tasks delete

--- src/ex2_js-basics-part2/task_01.js
+++
@@ -1,9 +0,0 @@
-function typeofArguments(a) {
-    if (typeof a == "number") 
-        return " This is number";
-    else if (typeof a == "string")
-        return "This is string";
-    else 
-        return "Underfind";
-}
-console.log (typeofArguments(45));
