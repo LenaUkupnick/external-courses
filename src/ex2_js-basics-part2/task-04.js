66e1f2d5067afc486f88a8cfef604b4ebd89f591
LenaUkupnick <43740817+LenaUkupnick@users.noreply.github.com>
2018-10-11 23:56:12 +0300

    Update task-04.js

--- src/ex2_js-basics-part2/task-04.js
+++ src/ex2_js-basics-part2/task-04.js
@@ -2,10 +2,12 @@
 var i = 0;
 function compareElemens(array) {
     for (i; i < array.length; i++) {
-      if (array[i++] !== array[i]) {
+      if (array[i] === array[i++]) {
         return false;
       }   
     }
+
     return true;
   }
+
   module.exports = compareElemens;
