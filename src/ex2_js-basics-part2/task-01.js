461a96fe6cf80b6a7354424d0944c2d146c8e2ed
LenaUkupnick <43740817+LenaUkupnick@users.noreply.github.com>
2018-10-11 23:35:32 +0300

    Update task-01.js

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -1,4 +1,5 @@
 'use strict'
+var a;
 function typeofArguments(a) {
     if (typeof a === "number"){ 
         return "number";
@@ -9,5 +10,5 @@ function typeofArguments(a) {
    
     return undefined;
 }
-console.log (typeofArguments(null));
+
 module.exports = typeofArguments;
