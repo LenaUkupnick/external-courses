b40143cd624fa2079116386b336ebea1c8123fcb
Lena Barinova <ukupnickonelove@gmail.com>
2018-10-11 13:22:26 +0300

    task-01.js is ready

--- src/ex2_js-basics-part2/task-01.js
+++ src/ex2_js-basics-part2/task-01.js
@@ -1,11 +1,13 @@
-var a = (null);
+
 function typeofArguments(a) {
-    if (typeof a == "number") 
+    if (typeof a === "number"){ 
         return "number";
-    else if (typeof a == "string")
+    }
+    else if (typeof a === "string") {
         return "string";
-        else if ( typeof a == "otherwise")
-        return "underfined";
+    }
+   
+    return undefined;
 }
-console.log (typeofArguments(35));
+console.log (typeofArguments(null));
 module.exports = typeofArguments;
