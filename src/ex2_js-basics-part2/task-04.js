845fcd79ee0a8e520c5dfa1a9f91984729ee55a5
LenaUkupnick <43740817+LenaUkupnick@users.noreply.github.com>
2018-10-11 23:51:45 +0300

    Update task-04.js

--- src/ex2_js-basics-part2/task-04.js
+++ src/ex2_js-basics-part2/task-04.js
@@ -2,7 +2,7 @@
 var i = 0;
 function compareElemens(array) {
     for (i; i < array.length; i++) {
-      if (array[1] !== array[0]) {
+      if (array[i++] !== array[0]) {
         return false;
       }   
     }
