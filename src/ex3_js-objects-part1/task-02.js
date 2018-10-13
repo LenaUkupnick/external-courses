fc911561b412c723d7fe31149f0689d404138e1f
LenaUkupnick <43740817+LenaUkupnick@users.noreply.github.com>
2018-10-13 16:52:12 +0300

    Update task-02.js

--- src/ex3_js-objects-part1/task-02.js
+++ src/ex3_js-objects-part1/task-02.js
@@ -7,6 +7,7 @@ var key;
     keys (human)
     function keys(human) { 
         for (key in human){
+            if (human.hasOwnProperty(key)){
             console.log(key + ":" + human[key]);
         }      
     } 
