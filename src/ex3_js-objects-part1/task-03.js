ad5c0b4c313820e79d6b4b44cbc37e3ccb12b011
LenaUkupnick <43740817+LenaUkupnick@users.noreply.github.com>
2018-10-13 16:20:32 +0300

    Update task-03.js

--- src/ex3_js-objects-part1/task-03.js
+++ src/ex3_js-objects-part1/task-03.js
@@ -1,9 +1,3 @@
-// function doesPropertyExist(propertyName, object) {
-//     return object[propertyName] != undefined
-//     }
-    
-//    console.log (doesPropertyExist("pussy", {pussy:5}))
-//     console.log (doesPropertyExist("pusy", {pussy:5}))
     'use strict'
     var obj;
     var name;
