cbc662fa2d21b708f3d5ad31878570a6650cfb89
LenaUkupnick <43740817+LenaUkupnick@users.noreply.github.com>
2018-10-21 16:37:20 +0300

    Update task-02.js

--- src/ex2_js-basics-part2/task-02.js
+++ src/ex2_js-basics-part2/task-02.js
@@ -1,8 +1,1 @@
-var arr = [1, 2];
-function outputElements (arr) {
-    for (i = 0; i < arr.length; i++){
-        console.log (arr[i]);
-    }
-        console.log(arr.length)}
 
-        module.exports = outputElements;
