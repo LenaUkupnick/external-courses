0bc59973c61d6a79d5d8040bb9d41f96bf06a501
Lena Barinova <ukupnickonelove@gmail.com>
2018-10-21 16:23:45 +0300

    task-10.js  is done

---
+++ src/ex4_js-objects-part2/task-12.js
@@ -0,0 +1,12 @@
+"use strict"
+var a = 2;
+var b = 1.3333;
+var c;
+function sumNumbers (a, b) {
+    var c = a + b;
+    return (+c.toFixed(3))
+}
+console.log (c)
+
+
+module.exports = sumNumbers;
