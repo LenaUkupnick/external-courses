2fa51ac1307a73f8403fcfaef1ad0e6b3b86e38b
Lena Barinova <ukupnickonelove@gmail.com>
2018-10-21 16:26:50 +0300

    task-12.js  is done

--- src/ex4_js-objects-part2/task-12.js
+++ src/ex4_js-objects-part2/task-12.js
@@ -4,9 +4,7 @@ var b = 1.3333;
 var c;
 function sumNumbers (a, b) {
     var c = a + b;
-    return (+c.toFixed(3))
+    return (+c.toFixed(3));
 }
-console.log (c)
-
-
+console.log (c);
 module.exports = sumNumbers;
