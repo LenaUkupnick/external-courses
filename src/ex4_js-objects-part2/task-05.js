75a6e4a7101d08748543f3138de388a53e8f9d4b
Lena Barinova <ukupnickonelove@gmail.com>
2018-10-21 15:59:54 +0300

    task-05.js is done

---
+++ src/ex4_js-objects-part2/task-05.js
@@ -0,0 +1,12 @@
+"use strict"
+var str;
+var str2;
+function findString(str,str2) {
+    if (~ str.indexOf(str2)) {
+        return true;
+    } 
+    else if (str === " ");
+    return false;
+    ;
+}
+module.exports = findString;
