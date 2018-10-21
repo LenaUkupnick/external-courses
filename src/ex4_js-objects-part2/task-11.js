ced0731ff0e79b8b547b13513c3d63ec27195020
LenaUkupnick <43740817+LenaUkupnick@users.noreply.github.com>
2018-10-21 16:56:00 +0300

    Update task-11.js

--- src/ex4_js-objects-part2/task-11.js
+++ src/ex4_js-objects-part2/task-11.js
@@ -2,6 +2,7 @@
 var arr; 
 var result = {};
 var i = 0; 
+var key;
 function howManyTimes(str) { 
  arr = str.split(''); 
     for (i; i < arr.length; i++) { 
@@ -12,7 +13,7 @@ function howManyTimes(str) {
           result[arr[i]] = 1; 
         } 
     }
-  for (var key in result) { 
+  for  (key in result) { 
         if (result.hasOwnProperty(key)){ 
     console.log(key + ' - ' + result[key]); 
         } 
