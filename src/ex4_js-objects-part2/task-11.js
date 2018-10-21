92d709163e75473d3d982e56cba815aa00ab0ed7
LenaUkupnick <43740817+LenaUkupnick@users.noreply.github.com>
2018-10-21 16:48:24 +0300

    Update task-11.js

--- src/ex4_js-objects-part2/task-11.js
+++ src/ex4_js-objects-part2/task-11.js
@@ -1,18 +1,18 @@
 'use strict' 
 var arr; 
-var result;
+var result = {};
 var i = 0; 
-var key; 
 function howManyTimes(str) { 
- arr = str.split(' '); 
- result = {}
+ arr = str.split(''); 
     for (i; i < arr.length; i++) { 
         if (result[arr[i]] !== undefined) { 
             result[arr[i]]++ ; 
         }    
-        else result[arr[i]] = 1; 
+        else {
+          result[arr[i]] = 1; 
         } 
-    for (key in result) { 
+    }
+  for (var key in result) { 
         if (result.hasOwnProperty(key)){ 
     console.log(key + ' - ' + result[key]); 
         } 
