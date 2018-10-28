3c7978a3231531963a28a7b096731daf4509fe26
Lena Barinova <ukupnickonelove@gmail.com>
2018-10-28 21:19:26 +0300

    result conflict

---
+++ src/ex2_js-basics-part2/task-02.js
@@ -0,0 +1,10 @@
+'use strict'
+var arr = [1, 2];
+var i = 0;
+function outputElements (arr) {
+    for (i; i < arr.length; i++){
+        console.log (arr[i]);
+    }
+        console.log(arr.length)}
+
+        module.exports = outputElements;
---
+++ src/ex2_js-basics-part2/task-05.js
@@ -0,0 +1,14 @@
+'use strict'
+var arr = [1, -100, 100];
+var max = 0;
+var i = 0;
+function maxElement(arr) {
+    for(i; i < arr.length; i++ ){
+        if (arr [i] > max){
+            max = arr [i];
+        }
+    }
+    return max;
+}
+
+module.exports = maxElement;
