aaad29c9d3014713ff7f1c3051d92c02a2193941
Lena Barinova <ukupnickonelove@gmail.com>
2018-11-17 13:20:40 +0300

    tasks is done

--- src/ex6_js-array-methods/task-01.js
+++ src/ex6_js-array-methods/task-01.js
@@ -1,35 +1,35 @@
-'use strict' 
-var i; 
-var array; 
-    function sliceAnalogue (arr, num, num1) { 
-        array = []; 
-            if (num === undefined && num1 === undefined) { 
-                for (i = 0; i < arr.length; i++) { 
-                    array.push(arr[i]); 
-                } 
-                return array; 
-            } 
-            if (num1 === undefined && num > 0) { 
-                for (i = num; i < arr.length; i++) { 
-                    array.push(arr[i]); 
-                } 
-                return array; 
-            } 
-            if (num < 0 && num1 === undefined) { 
-                for (i = (num + arr.length); i < arr.length; i++) { 
-                    array.push(arr[i]); 
-                } 
-                return array; 
-            } 
-            if (num < 0 && num1 < 0) { 
-                for (i = (num + arr.length); i < (num1 + arr.length); i++) { 
-                   array.push(arr[i]); 
-                } 
-                    return array; 
-                } 
-                for (i = num; i < num1; i++) { 
-                    array.push(arr[i]); 
-                } 
-                    return array; 
-    }; 
+'use strict'
+var i;
+var array;
+function sliceAnalogue(arr, num, num1) {
+    array = [];
+    if (num === undefined && num1 === undefined) {
+        for (i = 0; i < arr.length; i++) {
+            array.push(arr[i]);
+        }
+        return array;
+    }
+    if (num1 === undefined && num > 0) {
+        for (i = num; i < arr.length; i++) {
+            array.push(arr[i]);
+        }
+        return array;
+    }
+    if (num < 0 && num1 === undefined) {
+        for (i = (num + arr.length); i < arr.length; i++) {
+            array.push(arr[i]);
+        }
+        return array;
+    }
+    if (num < 0 && num1 < 0) {
+        for (i = (num + arr.length); i < (num1 + arr.length); i++) {
+            array.push(arr[i]);
+        }
+        return array;
+    }
+    for (i = num; i < num1; i++) {
+        array.push(arr[i]);
+    }
+    return array;
+};
 module.exports = sliceAnalogue;
--- src/ex6_js-array-methods/task-02.js
+++ src/ex6_js-array-methods/task-02.js
@@ -2,11 +2,11 @@
 var i = 0;
 var arr;
 function someAnalogue(arr, callback) {
-    for ( i; i < arr.length; i++) {
+    for (i; i < arr.length; i++) {
         if (callback(arr[i], i, arr)) {
-            return true;  
-        }           
-    } 
+            return true;
+        }
+    }
     return false;
 }
 module.exports = someAnalogue;
