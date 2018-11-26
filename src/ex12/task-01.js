5d8246513431858583dde24ae037b7d08484f7a7
Lena Barinova <ukupnickonelove@gmail.com>
2018-11-26 13:00:05 +0300

    tasks are fixed

--- src/ex12/task-01.js
+++ src/ex12/task-01.js
@@ -1,12 +1,18 @@
-
-document.getElementById('accordeon').addEventListener('click', function(elem) {
-    var clickedElement = elem.target.closest('.accordeon-item');
+// document.getElementById("accordeon").addEventListener("click", function(elem) {
+//     var clickedElement = elem.target.closest(".accordeon-item");
+//     if (elem.target && elem.target.nodeName === "H3") {
+//         var elements = document.getElementsByClassName("active")[0];
+//         elements.classList.remove("active")
+// }
+//     var textInElement = clickedElement.getElementsByClassName("accordeon-text")[0];
+//     textInElement.classList.add("active");
+// }) 
+document.getElementById("accordeon").addEventListener("click", function(elem) {
+    var clickedElement = elem.target.closest(".accordeon-item");
     if (elem.target && elem.target.nodeName === "H3") {
-        var elements = document.getElementsByClassName("accordeon-text");
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].classList.remove('active');
-    }
-        var textInElement = clickedElement.getElementsByClassName('accordeon-text')[0];
-        textInElement.classList.add('active');
-    }
-})
+        var elements = document.getElementsByClassName("active")[0];
+        elements.classList.remove("active");
+        var textInElement = clickedElement.getElementsByClassName("accordeon-text")[0];
+    textInElement.classList.add("active");
+}   
+}) 
